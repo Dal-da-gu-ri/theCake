a09b59e51d7c914cb9fc3d6d097d388847f15b2b
yelcohol <57361719+yelcohol@users.noreply.github.com>
2020-10-19 00:17:13 +0900

    Update settings.py

--- theCake2/settings.py
+++ theCake2/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'theCake2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'theCake2/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
