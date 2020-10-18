30afd21c2b285df5122610f5384fad5e33265ef9
Sohyun Park <carriesohyun@gmail.com>
2020-10-18 23:27:32 +0900

    사업자 회원가입 과정 변경

--- baker/urls.py
+++ baker/urls.py
@@ -5,16 +5,17 @@ from . import views
 urlpatterns = [
     path('login/', views.login, name='login_baker'),
     path('idpw_search/', views.idpw_search, name='idpw_search_baker'),
-    path('signUp/', views.signUp, name='signUp_baker'),
+    path('signUp/crnCheck/', views.crnCheck, name='crnCheck'),
+    path('signUp/join/', views.join, name='join_baker'),
 
     # 가게 관리
-    path('manageStore/enrollStore', views.enrollStore, name='enrollStore_baker'),
-    path('manageStore/opendays', views.opendays, name='opendays_baker'),
-    path('manageStore/storeReview', views.storeReview, name='storeReview_baker'),
+    path('manageStore/enrollStore/', views.enrollStore, name='enrollStore_baker'),
+    path('manageStore/opendays/', views.opendays, name='opendays_baker'),
+    path('manageStore/storeReview/', views.storeReview, name='storeReview_baker'),
 
     # 케이크 관리
-    path('manageCake/myCakes', views.myCakes, name='myCakes_baker'),
-    path('manageCake/options', views.options, name='options_baker'),
+    path('manageCake/myCakes/', views.myCakes, name='myCakes_baker'),
+    path('manageCake/options/', views.options, name='options_baker'),
 
     # 주문 관리
     path('manageOrder/', views.manageOrder, name='manageOrder_baker'),
--- baker/views.py
+++ baker/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
-from home.models import Orderer, Order, Store, Baker, Review, Option, DetailedOption, Cake
+from django.shortcuts import render, redirect
+from home.models import Orderer, Order, Store, Baker, Review, Option, DetailedOption, Cake, checkBaker
 from django.contrib.auth.models import User
 from django.contrib import auth
+from django.contrib.auth.hashers import make_password, check_password
+from .crn import Search_CRN
 
 #make_password(str) : 이 함수에 넣어준 문자열을 암호화합니다. (hashing)
 #check_password(a,b) : a,b가 일치하는지 확인, 반환합니다.
-from django.contrib.auth.hashers import make_password, check_password
 
 ################################
 # 이메일 인증 백엔드 구역이래요!!!! #
@@ -14,23 +15,93 @@ from django.contrib.auth.hashers import make_password, check_password
 from django.http import HttpResponse, JsonResponse
 # Create your views here.
 
-def signUp(request):
-    if request.method == 'POST':
-        username = request.POST.get['']
-        # 비밀번호 이미 확인된 상태라 필요없음.
-        if request.POST["password_baker"] == request.POST["re-password_baker"]:
-            bakerUser = Baker.objects.create()
-
-            bakerUser.save()
-    return render(request, 'baker/signUp_baker.html')
-
+def join(request):
+    #global bsID, emailBaker
+    if request.method == "GET":
+        return render(request, 'baker/join_baker.html')
+    elif request.method == "POST":
+        businessName = request.POST.get('businessName',None)
+        namebaker = request.POST.get('name_baker',None)
+        emailbaker = request.POST.get('email_baker',None)
+        passwordbaker = request.POST.get('password_baker',None)
+        phoneNumbaker = request.POST.get('phoneNum_baker',None)
+        res_data = {}
+        try:
+            curBaker = checkBaker.objects.get(businessName = businessName)
+            crnNum = curBaker.businessCRN
+            baker = Baker(
+                businessID=crnNum,
+                businessName=businessName,
+                email=emailbaker,
+                name=namebaker,
+                phoneNum=phoneNumbaker,
+                password=make_password(passwordbaker)
+            )
+            baker.save()
+            return redirect('/baker/login')
+            #return render(request, 'fuser/login.html')
+        except checkBaker.DoesNotExist:
+            # comment = None
+            res_data['error'] = "등록되지 않은 사업자명입니다."
+            return render(request, 'baker/join_baker.html', res_data)
 
 
 def login(request):
-    #person = Baker
-    #person.email = request.GET('email')
-    #person.password = request.GET('password')
-    return render(request, 'baker/login_baker.html')
+    if request.method=="GET":
+        return render(request,'baker/login_baker.html')
+    elif request.method == "POST":
+        #전송받은 이메일 비밀번호 확인
+        businessID = request.POST.get('businessID')
+        password = request.POST.get('password')
+
+        #유효성 처리
+        res_data={}
+        if Baker.objects.filter(businessID=businessID).exists():
+            #res_data['error']="모든 칸을 다 입력해주세요"
+            baker = Baker.objects.get(businessID=businessID)
+            if check_password(password,baker.password):
+                #응답 데이터 세션에 값 추가. 수신측 쿠키에 저장됨
+                #request.session['user']=baker.id
+                request.session['user']=baker.businessID
+
+                #리다이렉트
+                return redirect('/') #urls에 정의해둔 home으로
+                #return render(request,'fuser/loginsuccess.html',res_data)
+            else:
+                res_data['error'] = "비밀번호가 틀렸습니다."
+                return render(request, 'baker/login_baker.html', res_data)
+
+        else:
+            res_data['error'] = "존재하지 않는 사업자번호입니다."
+            return render(request,'baker/login_baker.html',res_data)
+
+def crnCheck(request):
+    #global bsID
+    if request.method == "GET":  # url을 이용한 방법
+        return render(request, 'baker/crnCheck.html')
+    elif request.method == "POST":  # 등록 버튼을 사용한 방법기
+        businessName = request.POST.get('businessName',None)
+        crn = request.POST.get('businessID', None)
+        res_data = {}
+        if Search_CRN(crn) == "부가가치세 일반과세자 입니다.":
+            try:
+                baker = checkBaker.objects.get(businessCRN = crn)
+                res_data['error'] = "이미 등록된 사업자등록번호입니다."
+                return render(request, 'baker/crnCheck.html', res_data)
+            except checkBaker.DoesNotExist:
+                #comment = None
+                checkbaker = checkBaker(
+                    businessname=businessName,
+                    businessCRN=crn
+                )
+                checkbaker.save()
+                return redirect('/baker/signUp/join')
+                #return render(request, 'baker/join_baker.html')
+
+
+        else:
+            res_data['error'] = "유효하지 않은 사업자등록번호입니다."
+            return render(request, 'baker/crnCheck.html', res_data)
 
 def idpw_search(request):
     return render(request, 'baker/idpw_search_baker.html')
