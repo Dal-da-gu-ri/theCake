48a1966e72892f7166217541e06b4025feddbb90
yelcohol <57361719+yelcohol@users.noreply.github.com>
2020-10-30 01:26:44 +0900

    Update forms.py

--- baker/forms.py
+++ baker/forms.py
@@ -1,12 +1,13 @@
 from django import forms
 
-from home.models import Store, Baker, Cake
+from home.models import Store, Baker, Cake, DetailedOption
 
 class StoreForm(forms.ModelForm):
 
     class Meta:
         model = Store
-        fields = ('businessID','storeName','storeContact','pickUpOpen','pickUpClose','aboutStore',)
+        fields = ['storeName', 'storeContact', 'pickUpOpen', 'pickUpClose', 'aboutStore']
+        # bussinessID 지웠음.
 
 class CakeForm(forms.ModelForm):
     class Meta:
@@ -16,5 +17,25 @@ class CakeForm(forms.ModelForm):
             'cakeName' : forms.TextInput(
                 # attrs={'class' : 'form-control', 'aria-label': 'cakeName', 'aria-describedby' : 'add-btn'}
                 # 여기를 이용해서 꾸며야 합니다.
+            )
+        }
+
+class DetailedOptionForm(forms.ModelForm):
+    class Meta:
+        model = DetailedOption
+        fields = ['detailName', 'pricing', 'withColor', 'withImage']
+        widgets = {
+            'detailName' : forms.TextInput(
+                attrs={'class' : 'form-control', 'aria-label': 'cakeName', 'aria-describedby' : 'add-btn'}
+                # 여기를 이용해서 꾸며야 합니다.
+            ),
+            'pricing' : forms.TextInput(
+                attrs={}
+            ),
+            'withColor' : forms.TextInput(
+                attrs={}
+            ),
+            'withImage' : forms.TextInput(
+                attrs={}
             )
         }
