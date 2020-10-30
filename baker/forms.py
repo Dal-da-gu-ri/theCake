ee280287c0e08ad60fea53fc3243f8f53516ef92
yelcohol <57361719+yelcohol@users.noreply.github.com>
2020-10-30 17:55:18 +0900

    Update forms.py

--- baker/forms.py
+++ baker/forms.py
@@ -2,28 +2,48 @@ from django import forms
 
 from home.models import Store, Baker, Cake, DetailedOption
 
-
 class StoreForm(forms.ModelForm):
-
     class Meta:
         model = Store
-        fields = ['storeName', 'storeContact', 'pickUpOpen', 'pickUpClose', 'aboutStore']
-        # bussinessID 지웠음. 더 추가하기
+        fields = ['storeName', 'storeContact', 'pickUpOpen', 'pickUpClose', 'aboutStore', 'storeImg', 'location']
+        widgets = {
+            'storeName': forms.TextInput(
+                label='가게 이름',
+                attrs={'class': 'form-control'}
+            ),
+            'storeContact': forms.TextInput(
+                label='가게 연락처',
+                attrs={'class: form-control'}
+            ),
+            'pickUpOpen': forms.TextInput(
+                label='픽업 오픈 시간',
+                attrs={'class: form-control'}
+            ),
+            'pickUpClose': forms.TextInput(
+                label='픽업 마감 시간',
+                attrs={'class: form-control'}
+            ),
+            'aboutStore': forms.Textarea(
+                label='가게 소개글',
+                attrs={'class: form-control'}
+            ),
+            'storeImg': forms.ImageField,
+            'location': forms.TextInput(
+                label='가게 위치',
+                attrs={'class: form-control'}
+            )
+        }
 
 class CakeForm(forms.ModelForm):
     class Meta:
         model = Cake
-        fields = ['cakeName', 'cakeImg', 'cakePrice', 'mini'] #여기서 cakeImg는 base64형식이어야 함
-        # widgets = {
-        #     'cakeName': forms.TextInput(
-        #         # attrs={'class' : 'form-control', 'aria-label': 'cakeName', 'aria-describedby' : 'add-btn'}
-        #         # 여기를 이용해서 꾸며야 합니다.
-        #     )
-        # }
-
-
-
-
+        fields = ['cakeName', 'cakeImg', 'cakePrice', 'mini']
+        widgets = {
+            'cakeName': forms.TextInput(
+                # attrs={'class' : 'form-control', 'aria-label': 'cakeName', 'aria-describedby' : 'add-btn'}
+                # 여기를 이용해서 꾸며야 합니다.
+            )
+        }
 
 class DetailedOptionForm(forms.ModelForm):
     class Meta:
