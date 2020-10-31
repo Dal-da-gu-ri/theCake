75842144aacd5ff65e9fa6595a07e5725bec8213
yelcohol <57361719+yelcohol@users.noreply.github.com>
2020-10-31 16:46:54 +0900

    combo box 다시 forms에 넣음

--- baker/forms.py
+++ baker/forms.py
@@ -15,11 +15,11 @@ class StoreForm(forms.ModelForm):
                 #label='가게 연락처',
                 attrs={'class': 'form-control'}
             ),
-            'pickUpOpen': forms.TextInput(
+            'pickUpOpen': forms.Select(
                 #label='픽업 오픈 시간',
                 attrs={'class': 'form-control'}
             ),
-            'pickUpClose': forms.TextInput(
+            'pickUpClose': forms.Select(
                 #label='픽업 마감 시간',
                 attrs={'class': 'form-control'}
             ),
