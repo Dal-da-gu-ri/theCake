3ca9ac2ec59f9760152327464b10f9c06a7d583a
yelcohol <57361719+yelcohol@users.noreply.github.com>
2020-11-08 15:00:34 +0900

    dayhandle, weekhandle css 적용 완료, base_baker에서 nav 수정

--- baker/forms.py
+++ baker/forms.py
@@ -146,6 +146,319 @@ class DailyAmountForm(forms.ModelForm):
         model = DailyAmount
         fields = ['day1','day2','day3','day4','day5','day6','day7','day8','day9','day10','day11','day12','day12','day13','day14','day15','day16','day17','day18','day19','day20','day21','day22','day23','day24','day25','day26','day27','day28','day29','day30','day31',
                   'day32','day33','day34','day35','day36','day37','day38','day39','day40','day41','day42','day43','day44','day45','day46','day47','day48','day49','day50','day51','day52','day53','day54','day55','day56','day57','day58','day59','day60','day61','day62']
+        widgets = {
+            'day1': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day2': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day3': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day4': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day5': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day6': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day7': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day8': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day9': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day10': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day11': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day12': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day13': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day14': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day15': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day16': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day17': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day18': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day19': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day20': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day21': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day22': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day23': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day24': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day25': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day26': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day27': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day28': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day29': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day30': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day31': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day32': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day33': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day34': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day35': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day36': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day37': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day38': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day39': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day40': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day41': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day42': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day43': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day44': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day45': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day46': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day47': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day48': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day49': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day50': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day51': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day52': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day53': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day54': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day55': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day56': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day57': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day58': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day59': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day60': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day61': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            ),
+            'day62': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-md-8"
+                }
+            )
+
+        }
 
 class DetailedOptionForm(forms.ModelForm):
     class Meta:
