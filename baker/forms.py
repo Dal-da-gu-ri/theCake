94012c025775a3d6715c86f76ddd5b3146fe202c
yelcohol <57361719+yelcohol@users.noreply.github.com>
2020-11-08 22:45:43 +0900

    Update forms.py //BakerForm 주석

--- baker/forms.py
+++ baker/forms.py
@@ -1,39 +1,39 @@
 from django import forms
 from home.models import *
 
-class BakerForm(forms.ModelForm):
-    class Meta:
-        model = Baker
-        fields = ['userID', 'name', 'phoneNum']
-        widgets = {
-            'userID':forms.TextInput(
-                attrs={'class':'form-control',
-                       'id': 'userID',
-                       'placeholder':'기입하였던 아이디를 입력하세요'}
-            ),
-            # 'email': forms.EmailField(
-            #     attrs={'class': 'form-control',
-            #            'id': 'email',
-            #            'placeholder': '이메일'}
-            # ),
-            'name': forms.TextInput(
-                attrs={'class': 'form-control',
-                       'id': 'name',
-                       'placeholder': '성명'}
-            ),
-            'phoneNum': forms.TextInput(
-                attrs={'class': 'form-control',
-                       'id': 'phoneNum',
-                       'placeholder': '예) 010-1234-5678',
-                       'pattern': "(010)-\d{3,4}-\d{4}"}
-            ),
-            # 'password': forms.PasswordInput(
-            #     attrs={'class': 'form-control',
-            #            'id': 'password',
-            #            'placeholder': '비밀번호'}
-            # )
-
-        }
+# class BakerForm(forms.ModelForm):
+#     class Meta:
+#         model = Baker
+#         fields = ['userID', 'name', 'phoneNum']
+#         widgets = {
+#             'userID':forms.TextInput(
+#                 attrs={'class':'form-control',
+#                        'id': 'userID',
+#                        'placeholder':'기입하였던 아이디를 입력하세요'}
+#             ),
+#             # 'email': forms.EmailField(
+#             #     attrs={'class': 'form-control',
+#             #            'id': 'email',
+#             #            'placeholder': '이메일'}
+#             # ),
+#             'name': forms.TextInput(
+#                 attrs={'class': 'form-control',
+#                        'id': 'name',
+#                        'placeholder': '성명'}
+#             ),
+#             'phoneNum': forms.TextInput(
+#                 attrs={'class': 'form-control',
+#                        'id': 'phoneNum',
+#                        'placeholder': '예) 010-1234-5678',
+#                        'pattern': "(010)-\d{3,4}-\d{4}"}
+#             ),
+#             # 'password': forms.PasswordInput(
+#             #     attrs={'class': 'form-control',
+#             #            'id': 'password',
+#             #            'placeholder': '비밀번호'}
+#             # )
+#
+#         }
 
 class StoreForm(forms.ModelForm):
     class Meta:
