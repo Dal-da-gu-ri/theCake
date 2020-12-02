afe88076ab69debdd16d6be2e7ae826f8945e20d
yelcohol <57361719+yelcohol@users.noreply.github.com>
2020-12-03 01:53:47 +0900

    Update settings.py

--- theCake2/settings.py
+++ theCake2/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.180.85.13']
+ALLOWED_HOSTS = ['15.164.100.127']
 
 SECRET_KEY = my_settings.SECRET_KEY
 
