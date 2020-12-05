9f02dc4f49537407b5c9332c4dd56f27ff4da78e
yelcohol <57361719+yelcohol@users.noreply.github.com>
2020-12-05 20:14:36 +0900

    day31 수정

--- baker/forms.py
+++ baker/forms.py
@@ -191,7 +191,7 @@ class OpenDaysForm(forms.ModelForm):
 class DailyAmountForm(forms.ModelForm):
     class Meta:
         model = DailyAmount
-        fields = ['day1','day2','day3','day4','day5','day6','day7','day8','day9','day10','day11','day12','day12','day13','day14','day15','day16','day17','day18','day19','day20','day21','day22','day23','day24','day25','day26','day27','day28','day29','day30']
+        fields = ['day1','day2','day3','day4','day5','day6','day7','day8','day9','day10','day11','day12','day12','day13','day14','day15','day16','day17','day18','day19','day20','day21','day22','day23','day24','day25','day26','day27','day28','day29','day30','day31']
         widgets = {
             'day1': forms.NumberInput(
                 attrs={
@@ -373,6 +373,12 @@ class DailyAmountForm(forms.ModelForm):
                 attrs={
                     'class': "text-center form-group col-12 col-md-10 mt-3",
                     'id': 'day30'
+                }
+            ),
+            'day31': forms.NumberInput(
+                attrs={
+                    'class': "text-center form-group col-12 col-md-10 mt-3",
+                    'id': 'day31'
                 }
             )
 
